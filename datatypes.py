a627251434925ce2478a2b87868b2ca02da8a7ff
HD88-max <hardikdhallam@gmail.com>
2024-12-23 10:40:23 +0000

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+name = "hi"
+print(name)
+x = type(name)
+print("the data type of name is" ,x)
+
+num = 24
+print(num)
+
+x = type(num)
+print("the data type of num is" ,x)
+
+num1 = 12.556
+print("the data type of num1 is" ,type(num1))
+
+x = True
+y = False
+
+print("the data type of x is" ,type(x))
+print("the data type of y is" ,type(y))
+
---
+++ typecasting.py
@@ -0,0 +1,16 @@
+x = "5"
+x = int(x)
+print(type(x))
+x = float(x)
+print(type(x))
+x = int(x)
+print(type(x))
+x = str(x)
+print(type(x))
+
+age = int(input("What is your age? "))
+
+age = int(age + 5)
+print(age)
+age = float(age)
+print(age)
